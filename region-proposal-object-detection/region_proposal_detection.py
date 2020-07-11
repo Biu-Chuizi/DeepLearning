15f9b8b076bd6aab26e0aa5fa05455c657133a2a
John-Joe <guozhe123@outlook.com>
2020-07-11 13:50:52 +0900

    Update region_proposal_detection.py

--- region-proposal-object-detection/region_proposal_detection.py
+++ region-proposal-object-detection/region_proposal_detection.py
@@ -1,5 +1,11 @@
-# USAGE
-# python region_proposal_detection.py --image beagle.png
+#######################################################################################################
+# -*- coding:utf-8 -*-
+# File: region_proposal_detection.py
+# Update: 2020/07/11
+# USAGE: python region_proposal_detection.py --image beagle.png
+# Description: 通过控制单项personIdx=0读取特定种类，实现单项检测，和我们的原理相似
+########################################################################################################
+
 
 # import the necessary packages
 from tensorflow.keras.applications import ResNet50
